05f30f762ba0d4db9fbcef5835285a2aa80c8bc6
hangie <hangie@users.noreply.github.com>
2025-12-15 22:18:36 +0000

    deploy: 228c6362eea38d8a426ae3e8456815d9285eeb7a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev27+g9c91ce512',
+    VERSION: '1.0.1.dev30+g228c6362e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
